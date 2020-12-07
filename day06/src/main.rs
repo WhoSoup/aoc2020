ca431ae43479d19510e940d15ad43c4318b9c0e0
WhoSoup <who.soup@gmail.com>
2020-12-07 08:10:37 +0100

    simple day 6

---
+++ day06/src/main.rs
@@ -0,0 +1,45 @@
+use std::collections::{HashMap, HashSet};
+
+fn main() {
+    let content = std::fs::read_to_string("input.txt").unwrap();
+    let p1: u32 = content
+        .split("\n\n")
+        .map(
+            // a group
+            |x| {
+                x.split("\n")
+                    // a declaration
+                    .fold(HashSet::new(), |mut acc, answer| {
+                        acc.extend(answer.chars());
+                        acc
+                    })
+                    .len() as u32
+            },
+        )
+        .sum();
+
+    println!("Part One: {}", p1);
+
+    let p2: u32 = content
+        .split("\n\n")
+        .map(
+            // a group
+            |x| {
+                let decls = x.trim().split("\n");
+                let total = decls.clone().count() as u32;
+                decls
+                    .fold(vec![0; 26], |mut acc, answer| {
+                        for c in answer.chars() {
+                            acc[(c as i8 - 97) as usize] += 1;
+                        }
+                        acc
+                    })
+                    .iter()
+                    .filter(|x| **x == total)
+                    .count()
+            } as u32,
+        )
+        .sum();
+
+    println!("Part Two: {}", p2);
+}

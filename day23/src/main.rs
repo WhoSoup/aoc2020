de8e726ae11252efda0472941e91575856b01c10
WhoSoup <who.soup@gmail.com>
2020-12-31 08:53:59 +0100

    day 23

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,81 +1,73 @@
-use std::collections::VecDeque;
-
-fn play_game(cups: VecDeque<usize>, rounds: i32) -> VecDeque<usize> {
+fn play_game(start: Vec<usize>, rounds: i32) -> Vec<usize> {
+    let mut cups = vec![0; start.len()];
     let size = cups.len();
-    let mut cups = cups;
 
-    for _turn in 0..rounds {
-        /*println!(
-            "{}: {:?}",
-            turn,
-            cups.iter().map(|n| n + 1).collect::<Vec<usize>>()
-        );*/
-        let a = cups.remove(1).unwrap();
-        let b = cups.remove(1).unwrap();
-        let c = cups.remove(1).unwrap();
+    for n in 0..start.len() {
+        cups[start[n]] = start[(n + 1) % size];
+    }
 
-        let mut dest = (cups[0] + size - 1) % size;
-        //        println!("cups = {:?}, dest = {}", cups, dest);
+    let mut pos = start[0];
+
+    for _r in 0..rounds {
+        let a = cups[pos];
+        let b = cups[a];
+        let c = cups[b];
+
+        cups[pos] = cups[c];
+
+        let mut dest = (pos + size - 1) % size;
         while dest == a || dest == b || dest == c {
             dest = (dest + size - 1) % size;
         }
 
-        let dest_index = cups.iter().position(|&n| n == dest).unwrap() + 1;
+        cups[c] = cups[dest];
+        cups[dest] = a;
 
-        cups.insert(dest_index, c);
-        cups.insert(dest_index, b);
-        cups.insert(dest_index, a);
-
-        cups.rotate_left(1);
+        pos = cups[pos];
     }
-
-    let pos = cups.iter().position(|&n| n == 0).unwrap();
-    cups.rotate_left(pos);
 
     cups
 }
 
 fn part_one(cups: &str) {
-    let cups: VecDeque<usize> = cups
+    let cups: Vec<usize> = cups
         .chars()
         .map(|n| (n.to_digit(10).unwrap() - 1) as usize)
         .collect();
 
-    let result = play_game(cups, 100);
+    let result = play_game(cups.clone().into_iter().collect(), 100);
 
-    println!(
-        "Part One: {}",
-        result
-            .into_iter()
-            .skip(1)
-            .map(|n| (n + 1).to_string())
-            .collect::<Vec<String>>()
-            .join(""),
-    );
+    print!("Part One: ");
+    let mut start = 0;
+    for _ in 0..result.len() {
+        if start != 0 {
+            print!("{}", start + 1);
+        }
+        start = result[start];
+    }
+    println!();
 }
 
 #[allow(dead_code)]
 fn part_two(cups: &str) {
-    let mut cups: VecDeque<usize> = cups
+    let mut cups: Vec<usize> = cups
         .chars()
         .map(|n| (n.to_digit(10).unwrap() - 1) as usize)
         .collect();
 
-    for c in 9..10_000_000 {
-        cups.push_back(c);
+    for c in 9..1_000_000 {
+        cups.push(c);
     }
 
-    let result = play_game(cups, 1_000_000);
+    let result = play_game(cups, 10_000_000);
 
-    println!(
-        "Part Two: {} * {} = {}",
-        result[1],
-        result[2],
-        result[1] * result[2],
-    );
+    let a = result[0];
+    let b = result[a];
+
+    println!("Part Two: {} * {} = {}", a + 1, b + 1, (a + 1) * (b + 1));
 }
 
 fn main() {
     part_one("974618352");
-    //part_two("974618352");
+    part_two("974618352");
 }
